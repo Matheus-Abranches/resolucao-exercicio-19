5b624cd56d3c0caf18da8f5bc6a29f21b3afb7f7
xirovinha <xirova.abranches@outlook.com>
2024-07-08 13:17:37 -0300

    resolucao exercicio 19

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Tabuadas tabuadas = new Tabuadas();
+
+        tabuadas.tabuadas();
+        System.out.println(tabuadas);
+
+
+    }
+}
+
+
+
